d7d4742842fadd03ed403060dfbc3d9649dc0673
mayongxing <mayongxing@cmsr.chinamobile.com>
2023-02-06 22:45:42 +0800

    提交

--- handlers/user_msg_handler.go
+++ handlers/user_msg_handler.go
@@ -38,7 +38,7 @@ func (g *UserMessageHandler) ReplyText(msg *openwechat.Message) error {
 	reply, err := gtp.Completions(requestText)
 	if err != nil {
 		log.Printf("gtp request error: %v \n", err)
-		msg.ReplyText("机器人神了，我一会发现了就去修。")
+		msg.ReplyText("网络问题请稍后再试，或者练习沛公修复。")
 		return err
 	}
 	if reply == "" {
